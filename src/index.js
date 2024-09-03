445c7ea2b40f71983ea79428f48c02cbc217041e
mohamedalaa1197 <mohamed.mohdy1197@gmail.com>
2024-09-03 23:43:28 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+
+const PORT = 4000;
+const app = express();
+
+
+app.get('/', (req, res) => {    
+    res.send('<h1>Hello World! From Alaa 1211aas </h1>'); 
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+}); 
