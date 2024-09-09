6f03a3535512b69baf7f98a505b35f45b4f7410a
mohamedalaa1197 <mohamed.mohdy1197@gmail.com>
2024-09-09 15:10:38 +0300

    add docker volumes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
-
-
 const PORT = 4000;
 const app = express();
+var mongoose = require('mongoose');
+
+const username = 'root'
+const password = 'example'
+const port = '27017'
+
+var host = 'mongo'
+
+mongoose.connect(`mongodb://${username}:${password}@${host}:${port}`)
+.then(() => console.log('Connectted to MongoDB'))
+.catch(err => console.log('Error connecting to MongoDB, ', err)) ;
 
 
 app.get('/', (req, res) => {    
